daa1162d180f24f42db770804db324693795debf
astnreaperx <austin.slota@gmail.com>
2020-11-15 13:39:27 -0600

    Added fakker and Active Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "faker"
+AdminUser.delete_all
 ProductCategory.delete_all
 ProductOrder.delete_all
 
@@ -18,10 +20,39 @@ Order.delete_all
 Product.delete_all
 Woodworker.delete_all
 
-cat = Category.create(
-  name:        "Test",
-  description: "Test"
-)
+# Generate 4 Categories
+categories = ["Victorian", "Modern", "Vintage", "Traditional"]
+description = ["1", "2", "3", "4"]
+4.times do |c|
+  Category.create(
+    name:        categories[c],
+    description: description[c]
+  )
+end
+
+# Generate 10 WoodWorkers
+20.times do |w|
+  Woodworker.create(
+    user_name:  Faker::Games::Witcher.character,
+    first_name: Faker::Name.first_name,
+    last_name:  Faker::Name.last_name,
+    shop_name:  Faker::Space.moon,
+    address:    Faker::Address.full_address
+  )
+end
+
+# Generate 100 Products with woodworker and categories
+100.times do |p|
+  prod = Product.create(
+    name:          "teasdasd",
+    description:   "st",
+    price:         10,
+    delivery_cost: 10,
+    woodworker:    Woodworker.order("RANDOM()").first,
+    categories:    Category.order("RANDOM()").first(2)
+  )
+  puts prod.errors.messages
+end
 
 guy = Woodworker.create!(
   user_name:  "Test",
@@ -31,21 +62,18 @@ guy = Woodworker.create!(
   address:    "Test"
 )
 
-pro2 = guy.products.create!(
-  name:          "teasdasd",
-  description:   "st",
-  price:         10,
-  delivery_cost: 10
-)
+pro2 = guy.products.create!
 
 pro2.categories.create!(
   name:        "Test",
   description: "Test"
 )
-puts pro2.categories.first.name
 
-puts Product.count
-puts Woodworker.count
-puts Category.count
-puts ProductCategory.count
-puts ProductOrder.count
+puts "Product: #{Product.count}"
+puts "Woodworkers: #{Woodworker.count}"
+puts "Categories: #{Category.count}"
+puts "Product Categories: #{ProductCategory.count}"
+puts "Product Orders: #{ProductOrder.count}"
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
