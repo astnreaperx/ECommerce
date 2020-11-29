c6428e349fbb667888c3490599653667829023a6
Austin Slota <austin.slota@gmail.com>
2020-11-28 19:42:58 -0600

    Got multiple line items working

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,22 @@
 class CheckoutController < ApplicationController
     def create
         # Create new stripe request
-        @product = Product.find(params[:id])
+
+        @cart = Product.find(session[:cart])
+        @product = @cart[0]
+
+
+        @lineitems = []
+        @cart.each do |lineitem|
+            @lineitems <<
+            {
+                name: lineitem.name,
+                description: lineitem.description,
+                amount: (lineitem.price * 100  ),
+                currency: "cad",
+                quantity: 1
+            }
+        end
 
 
         if @product.nil?
@@ -12,22 +27,7 @@ class CheckoutController < ApplicationController
 
         @session = Stripe::Checkout::Session.create(
         payment_method_types: ["card"],
-        line_items:[
-            {
-            name: @product.name,
-            description: @product.description,
-            amount: (@product.price * 100  ),
-            currency: "cad",
-            quantity: 1
-            },
-            {
-                name: "GST",
-                description: "Taxes",
-                amount: (7),
-                currency: "cad",
-                quantity: 1
-            }
-        ],
+        line_items: @lineitems,
         success_url: checkout_success_url,
         cancel_url: checkout_cancel_url,
         )
